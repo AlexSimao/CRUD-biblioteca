565ae380ff95aeefc4b429fa1c5082fc17d7a1af
Alex <alex2003simao1@gmail.com>
2025-01-25 22:49:05 -0300

    Criado o método de consulta de um Autor especifico atraves do ID 'findById()'. Tambem foi feito um tratamento de exceção simples, caso seja feito uma requisição de um Autor que não consta na base de dados.

--- src/main/java/com/estudo/biblioteca/controllers/AutorController.java
+++ src/main/java/com/estudo/biblioteca/controllers/AutorController.java
@@ -1,15 +1,19 @@
 package com.estudo.biblioteca.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudo.biblioteca.dtos.AutorDTO;
+import com.estudo.biblioteca.entities.Autor;
+import com.estudo.biblioteca.repositories.AutorRepository;
 import com.estudo.biblioteca.services.AutorService;
 
 @RestController
@@ -19,11 +23,25 @@ public class AutorController {
   @Autowired
   private AutorService autorService;
 
+  @Autowired
+  private AutorRepository autorRepository;
+
   @GetMapping
   public ResponseEntity<List<AutorDTO>> getAll() {
     List<AutorDTO> result = autorService.getAll();
     return ResponseEntity.status(HttpStatus.OK).body(result);
 
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<?> findById(@PathVariable long id) {
+    Optional<Autor> optAutor = autorRepository.findById(id);
+    if (optAutor.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Autor não encontrado");
+    }
+
+    AutorDTO result = autorService.findById(id);
+    return ResponseEntity.status(HttpStatus.OK).body(result);
   }
 
 }
--- src/main/java/com/estudo/biblioteca/services/AutorService.java
+++ src/main/java/com/estudo/biblioteca/services/AutorService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.estudo.biblioteca.dtos.AutorDTO;
 import com.estudo.biblioteca.entities.Autor;
@@ -21,6 +22,13 @@ public class AutorService {
     List<Autor> result = autorRepository.findAll();
     // List<AutorDTO> dto = result.stream().map(autor -> new AutorDTO(autor)).toList();
     List<AutorDTO> dto = result.stream().map(AutorDTO::new).toList();
+    return dto;
+  }
+
+  @Transactional(readOnly = true)
+  public AutorDTO findById(@PathVariable long id) {
+    Autor result = autorRepository.findById(id).get();
+    AutorDTO dto = new AutorDTO(result);
     return dto;
   }
 
