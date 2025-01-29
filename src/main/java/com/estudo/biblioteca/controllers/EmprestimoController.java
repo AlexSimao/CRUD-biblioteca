f21d1931d7345ed08d915e58d39646d66e983e9b
Alex <alex2003simao1@gmail.com>
2025-01-29 20:19:15 -0300

    Criado od Services e Controllers da Entidade Emprestimo. Incluido os servicos de Emprestar e Devolver Livros.

---
+++ src/main/java/com/estudo/biblioteca/controllers/EmprestimoController.java
@@ -0,0 +1,51 @@
+package com.estudo.biblioteca.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudo.biblioteca.dtos.EmprestimoDTO;
+import com.estudo.biblioteca.services.EmprestimoService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping(value = "/emprestimo")
+public class EmprestimoController {
+
+  @Autowired
+  private EmprestimoService emprestimoService;
+
+  @GetMapping
+  public ResponseEntity<List<EmprestimoDTO>> findAll() {
+    List<EmprestimoDTO> result = emprestimoService.findAll();
+    return ResponseEntity.ok(result);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<EmprestimoDTO> findById(@PathVariable Long id) {
+    EmprestimoDTO result = emprestimoService.findById(id);
+    return ResponseEntity.ok(result);
+  }
+
+  @PostMapping
+  public ResponseEntity<EmprestimoDTO> novoEmprestimo(@RequestBody EmprestimoDTO emprestimoDTO) {
+    EmprestimoDTO result = emprestimoService.novoEmprestimo(emprestimoDTO);
+    return ResponseEntity.status(HttpStatus.CREATED).body(result);
+  }
+
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<EmprestimoDTO> devolucaoDeEmprestimo(@PathVariable Long id) {
+    EmprestimoDTO result = emprestimoService.devolucaoDeEmprestimo(id);
+    return ResponseEntity.ok(result);
+  }
+
+}
---
+++ src/main/java/com/estudo/biblioteca/services/EmprestimoService.java
@@ -0,0 +1,86 @@
+package com.estudo.biblioteca.services;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.estudo.biblioteca.dtos.EmprestimoDTO;
+import com.estudo.biblioteca.dtos.LivroDTO;
+import com.estudo.biblioteca.entities.Emprestimo;
+import com.estudo.biblioteca.entities.Livro;
+import com.estudo.biblioteca.repositories.EmprestimoRepository;
+
+@Service
+public class EmprestimoService {
+
+  @Autowired
+  private EmprestimoRepository emprestimoRepository;
+
+  @Autowired
+  private LivroService livroService;
+
+  private String getDate() {
+    String data = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+    return data;
+  }
+
+  private Livro toEntity(LivroDTO livroDTO) {
+    Livro entity = new Livro();
+    BeanUtils.copyProperties(livroDTO, entity);
+    return entity;
+  }
+
+  private Emprestimo toEntity(EmprestimoDTO emprestimoDTO) {
+    LivroDTO livro = livroService.findById(emprestimoDTO.getLivro_id());
+    Emprestimo emprestimo = new Emprestimo();
+    BeanUtils.copyProperties(emprestimoDTO, emprestimo);
+    emprestimo.setLivro(toEntity(livro));
+    return emprestimo;
+  }
+
+  @Transactional(readOnly = true)
+  public List<EmprestimoDTO> findAll() {
+    List<Emprestimo> result = emprestimoRepository.findAll();
+    List<EmprestimoDTO> dto = result.stream().map(EmprestimoDTO::new).toList();
+    return dto;
+  }
+
+  @Transactional(readOnly = true)
+  public EmprestimoDTO findById(@PathVariable Long id) {
+    Emprestimo result = emprestimoRepository.findById(id).get();
+    EmprestimoDTO dto = new EmprestimoDTO(result);
+    return dto;
+  }
+
+  @Transactional
+  public EmprestimoDTO novoEmprestimo(@RequestBody EmprestimoDTO emprestimoDTO) {
+    emprestimoDTO.setData_emprestimo(getDate());
+    Emprestimo result = emprestimoRepository.save(toEntity(emprestimoDTO));
+    EmprestimoDTO dto = new EmprestimoDTO(result);
+    return dto;
+  }
+
+  @Transactional
+  public EmprestimoDTO devolucaoDeEmprestimo(@PathVariable Long id) {
+    Emprestimo entity = emprestimoRepository.findById(id).get();
+
+    // Verifica se produto tem data de devolução.
+    if (entity.getData_devolucao() != null) {
+      return new EmprestimoDTO(entity);
+    }
+
+    entity.setData_devolucao(getDate());
+    Emprestimo result = emprestimoRepository.save(entity);
+    EmprestimoDTO dto = new EmprestimoDTO(result);
+    return dto;
+
+  }
+
+}
